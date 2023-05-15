ff1dd19ae62061dddd195d25b1ec64db368f1e05
Rahul O. R <rahulor@live.com>
2023-05-15 19:20:14 +0200

    deep n n using tensorflow

---
+++ deepnn.py
@@ -0,0 +1,11 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+print(tf.__version__)
+#tf.sysconfig.get_build_info() 
+
+import keras
+from tensorflow.keras import datasets, layers, models
+import pickle
+import helper
+import numpy as np
+
